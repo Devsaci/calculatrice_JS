1825d80cc347b0a7f543ac56c1adcc00449d1a19
saci zakaria <sacideveloppeur@gmail.com>
2020-08-19 13:14:07 +0200

    Etape 1 : Demander un mode de calcul

---
+++ code.js
@@ -0,0 +1,16 @@
+
+
+/* let Addition = 1;
+let Multiplication = 2;
+let Soustraction = 3 ;
+let Division = 4; */
+
+
+do {
+    
+var choix =prompt("Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n");
+
+
+
+
+} while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
