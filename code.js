3882857abbb5bfecde55cfb35e77aab00d98e812
saci zakaria <sacideveloppeur@gmail.com>
2020-08-21 11:17:00 +0200

    Etape 2 : Demander deux nombres à utiliser & vérifiez le type Number

--- code.js
+++ code.js
@@ -7,9 +7,10 @@ let Division = 4; */
 
 
 do {
-    
 var choix = Number(prompt("Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n")); 
-
-
-
 } while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
+
+do {
+    var premierNombre = Number(prompt("Entrez un premier Nombre")) ;
+    var deuxiemeNombre = Number(prompt("Entrez un deuxième Nombre")) ;
+} while (isNaN(premierNombre) || isNaN(deuxiemeNombre));
