c614ffdf3163e6dcf05eb91cc242c156f045be5f
saci zakaria <sacideveloppeur@gmail.com>
2020-08-21 11:50:45 +0200

    Etape 4 : Appelez les Fonctions à utiliser https://www.udemy.com/course/javascript-la-formation-ultime/learn/lecture/17163594#content

--- code.js
+++ code.js
@@ -1,35 +1,51 @@
+/*  Projet Calculatrice JAVASCRIPT
+    Auteur SACI ZAKARIA
+*/
 
-
-/* let Addition = 1;
-let Multiplication = 2;
-let Soustraction = 3 ;
-let Division = 4; */
-
-
-// Fonction Addition 
-function addition(nomnbreA,nomberB) {
-    return nomnbreA + nomnbreA;
-}
-// Fonction Soustraction
-function addition(nomnbreA,nomberB) {
-    return nomnbreA - nomnbreA;
-}
-// Fonction Multiplication
-function addition(nomnbreA,nomberB) {
-    return nomnbreA * nomnbreA;
-}
-// Fonction Division
-function addition(nomnbreA,nomberB) {
-    return nomnbreA / nomnbreA;
-}
-
-
+// etape 1 : Demandez un mode de calcul
 do {
 var choix = Number(prompt("Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n")); 
 } while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
 
+// Etape 2 : Demandez deux nombres à
 do {
     var premierNombre = Number(prompt("Entrez un premier Nombre")) ;
     var deuxiemeNombre = Number(prompt("Entrez un deuxième Nombre")) ;
 } while (isNaN(premierNombre) || isNaN(deuxiemeNombre));
 
+
+// Etape 3 : Creez les 4 fonctions
+// Fonction Addition 
+function addition(nomnbreA,nomberB) {
+    return nomnbreA + nomnbreA;
+}
+// Fonction Soustraction
+function soustraction(nomnbreA,nomberB) {
+    return nomnbreA - nomnbreA;
+}
+// Fonction Multiplication
+function multiplication(nomnbreA,nomberB) {
+    return nomnbreA * nomnbreA;
+}
+
+// Fonction Division
+function division(nomnbreA,nomberB) {
+    return nomnbreA / nomnbreA;
+}
+
+
+//Etape 4 : Appelez les Fonctions à utiliser
+switch (choix) {
+    case 1:
+        var resultat = addition(premierNombre,deuxiemeNombre)
+        break;
+    case 2:
+        var resultat = multiplication(premierNombre,deuxiemeNombre)
+        break;
+    case 3:
+        var resultat = soustraction(premierNombre,deuxiemeNombre)
+        break;
+    case 4:
+        var resultat = division(premierNombre,deuxiemeNombre)
+        break;
+}
