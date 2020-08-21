6bf5614eedb5cd13f6ce634f19e331edf5f0d132
saci zakaria <sacideveloppeur@gmail.com>
2020-08-21 11:26:29 +0200

    Etape 3 Creer les 4 fonctions https://www.udemy.com/course/javascript-la-formation-ultime/learn/lecture/17163816#content

--- code.js
+++ code.js
@@ -6,6 +6,24 @@ let Soustraction = 3 ;
 let Division = 4; */
 
 
+// Fonction Addition 
+function addition(nomnbreA,nomberB) {
+    return nomnbreA + nomnbreA;
+}
+// Fonction Soustraction
+function addition(nomnbreA,nomberB) {
+    return nomnbreA - nomnbreA;
+}
+// Fonction Multiplication
+function addition(nomnbreA,nomberB) {
+    return nomnbreA * nomnbreA;
+}
+// Fonction Division
+function addition(nomnbreA,nomberB) {
+    return nomnbreA / nomnbreA;
+}
+
+
 do {
 var choix = Number(prompt("Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n")); 
 } while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
@@ -14,3 +32,4 @@ do {
     var premierNombre = Number(prompt("Entrez un premier Nombre")) ;
     var deuxiemeNombre = Number(prompt("Entrez un deuxième Nombre")) ;
 } while (isNaN(premierNombre) || isNaN(deuxiemeNombre));
+
